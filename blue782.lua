34d4d1e0fdb8caf58dab050d2f751ec0cf7bea92
Nokiaff14 <cchirica748@gmail.com>
2025-10-26 01:00:39 +0300

    blue782 version 2

--- blue782.lua
+++ blue782.lua
@@ -1,31 +1,27 @@
 #!/usr/bin/bash
--- version 1
+-- version 2
 -- dont use, very unstable, tbh nobody sees that but still
 
-screen_click_1="select area" -- click to begin confirmation
-screen_click_2="confirm selected area" -- click again and confirm location
-
-print(screen_click_1)
-io.read()
-os.execute("xdotoo getmouselocation --shell > /tmp/pos1")
-local x1, y1 = 0, 0
-for line in io.lines("/tmp/pos1") do
-    if line:match("^X=") then x1 = line:match("X=(%d+)") end
-    if line:match("^Y=") then y1 = line:match("Y=(%d+)") end
+function wait_for_click(label)
+    print(label .. " (click anywhere...)")
+    os.execute("xdotool click 1")
+    os.execute("sleep 0.2")
+    os.execute("xdotool getmouselocation --shell > /tmp/pos")
+    local x, y = 0, 0
+    for line in io.lines("/tmp/pos") do
+        if line:match("^X=") then x = line:match("X=(%d+)") end
+        if line:match("^Y=") then x = line:match("Y=(%d+)") end
+    end
+    print("captured position: x=" .. x .. " y" .. y)
+    return x, Y
 end
-print("selected area: x=" .. x1 .. " y=" .. y1)
 
-print(screen_click_2)
-io.read()
-os.execute("xdotool getmouselocation --shell > /tmp/pos2")
-local x2, y2 = 0, 0
-for line in io.lines("tmp/pos2") do
-    if line:match("^X") then x2 = line:match("X=(%d+)") end
-    if line:match("^Y") then y2 = line:match("Y=(%d+)") end
-end
-print("confirmed area: x=" .. x2 .. " y=" .. y2)
+screen_click_1 = "select area"
+screen_click_2 = "confirm selected area"
 
-print("ready to click on (" .. x2 ..", " .. y2 .. ")")
-io.read()
+x1, y1 = wait_for_click(screen_click_1)
+x2, y2 = wait_for_click(screen_click_2)
+
+print("ready to click on(" .. x2 .. ", " .. y2 .. ")")
 os.execute("xdotool mousemove " .. x2 .. " " .. y2 .. " click 1")
 print("click done at x=" .. x2 .. " y=" .. y2)
