b7fea81618439d3c48b121773193d4e8b2592ee9
Nokiaff14 <cchirica748@gmail.com>
2025-10-26 00:16:59 +0300

    blue782 has entered da chat

---
+++ blue782.lua
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+-- version 1
+-- dont use, tbh nobody sees that but still
+
+screen_click_1="select area" -- click to begin confirmation
+screen_click_2="confirm selected area" -- click again and confirm location
+
